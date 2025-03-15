b83d33d302c886ccceaefcdfe24fc492fcbb2fb2
Daniel Maitz <57746489+dmaitz@users.noreply.github.com>
2025-03-15 19:18:44 +0100

    [fix] remove e- and p-core autodetection for now

--- ProcessorAffinityMgr.Service/ProcessorInformationReader.cs
+++ ProcessorAffinityMgr.Service/ProcessorInformationReader.cs
@@ -7,71 +7,25 @@ namespace ProcessorAffinityMgr.Service
     {
         public static bool GetCoreAffinityMasks()
         {
-            var length = 0;
-            GetLogicalProcessorInformationEx(0, IntPtr.Zero, ref length);
-            var buffer = Marshal.AllocHGlobal(length);
-            var pCoreMask = IntPtr.Zero;
-            var eCoreMask = IntPtr.Zero;
+            int logicalProcessorCount = Environment.ProcessorCount;
+            long pCoreMask = 0L;
+            long eCoreMask = 0L;
 
-            try
+            for (int i = 0; i < logicalProcessorCount; i++)
             {
-                if (GetLogicalProcessorInformationEx(0, buffer, ref length))
-                {
-                    var ptr = buffer;
-                    var offset = 0;
-                    var core = 0;
-                    long pMask = 0;
-                    long eMask = 0;
-
-                    while (offset < length)
-                    {
-                        var info = Marshal.PtrToStructure<SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX>(ptr);
-
-                        if (info.Processor.EfficiencyClass == 1)
-                            pMask |= 1L << core;
-                        else
-                            eMask |= 1L << core;
-
-                        core += 1;
-                        offset += info.Size;
-                        ptr = IntPtr.Add(buffer, offset);
-                    }
-
-                    if (pMask != 0) pCoreMask = (IntPtr)pMask;
-                    if (eMask != 0) eCoreMask = (IntPtr)eMask;
-                }
-            }
-            finally
-            {
-                Marshal.FreeHGlobal(buffer);
+                if (i < ProcessAffinityMgrService.Config.PCoreCount)
+                    pCoreMask |= (1L << i);
+                else
+                    eCoreMask |= (1L << i);
             }
 
-            ProcessAffinityMgrService.PCoreAffinityMask = pCoreMask;
-            ProcessAffinityMgrService.ECoreAffinityMask = eCoreMask;
+            ProcessAffinityMgrService.ServiceEventLog.WriteEntry($"P-Cores: {Convert.ToString(pCoreMask, 2)}\nE-Cores: {Convert.ToString(eCoreMask, 2)}");
+            
+            ProcessAffinityMgrService.PCoreAffinityMask = (IntPtr)pCoreMask;
+            ProcessAffinityMgrService.ECoreAffinityMask = (IntPtr)eCoreMask;
 
-            return pCoreMask == IntPtr.Zero;
+            return (pCoreMask == 0);
         }
 
-        [DllImport("kernel32.dll")]
-        private static extern bool
-            GetLogicalProcessorInformationEx(int relationshipType, IntPtr buffer, ref int length);
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX
-        {
-            public int Relationship;
-            public int Size;
-            public PROCESSOR_RELATIONSHIP Processor;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct PROCESSOR_RELATIONSHIP
-        {
-            public byte Flags;
-            public byte EfficiencyClass;
-            public byte Reserved1;
-            public byte Reserved2;
-            public IntPtr GroupMask;
-        }
     }
 }
