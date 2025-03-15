952c72ba7e0d5ac687bf9716414d76e9bdc7e3f4
Daniel Maitz <57746489+dmaitz@users.noreply.github.com>
2025-03-15 12:34:27 +0100

    Initial Version

---
+++ ProcessorAffinityMgr.Service/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace ProcessorAffinityMgr.Service
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ProcessAffinityService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
